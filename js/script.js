2fab10845c6f10ab4351134f6a141c2237d1f758
Alex Amodeo <famus123@hotmail.com>
2026-06-15 22:01:51 +0200

    cambiamento di sfile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+/* SLIDER PROMO */
+
+const slides = document.querySelectorAll(".promo-banner img");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+
+function nextSlide() {
+
+    currentSlide++;
+
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+
+if (slides.length > 1) {
+
+    showSlide(currentSlide);
+
+    setInterval(nextSlide, 5000);
+
+}
+
 /* MENU HAMBURGER */
 
 const menuToggle = document.querySelector(".menu-toggle");
